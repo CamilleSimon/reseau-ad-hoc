d89e533f63d2f9bbe6eb83d24f3dd45769b532ad
Camille Simon <37064032+CamilleSimon@users.noreply.github.com>
2018-06-19 09:15:16 +0200

    Update Movement.java

--- src/Movement.java
+++ src/Movement.java
@@ -4,23 +4,43 @@ import org.graphstream.graph.Node;
 import org.graphstream.ui.geom.Point3;
 import org.graphstream.ui.geom.Vector3;
 
+/**
+ * Classe Movement. Permet de gérer le déplacement des noeuds dans la simulation.
+ */
 public class Movement {
-	public Point3 pos = new Point3();
-	public Point3 target = new Point3();
-	public double speed;
-	protected Random random = new Random();
-
+	public Point3 pos = new Point3();		//Position du point sur la simulation
+	public Point3 target = new Point3();		//Position de la destination
+	public double speed;				//Vitesse de déplacement du point
+	protected Random random = new Random();		
+	
+	/**
+	 * Constructeur d'un Movement.
+	 * @param node Le noeud concerné
+	 * @param L Longueur de l'envorinnement
+	 * @param l Largeur de l'environnement
+	 * @param speed La vitesse de déplacement des noeuds 
+	 */
 	public Movement(Node node, int L, int l, double speed) {
 		this.speed = speed;
 		pos.set(random.nextDouble()*L, random.nextDouble()*l, 0);
 		target.set(random.nextDouble()*L, random.nextDouble()*l, 0);
 		node.addAttribute("xy", pos.x, pos.y);
 	}
-
+	
+	/**
+	 * Calcule la distance entre la position du Movement courant et un autre Movement passé en paramètre.
+	 * @param other  
+	 */
 	public  double distance(Movement other) {
 		return pos.distance(other.pos);
 	}
-
+	
+	/**
+	 * Déplace le noeud vers sa destination.
+	 * @param node Le noeud concerné
+	 * @param L Longueur de l'envorinnement
+	 * @param l Largeur de l'environnement
+	 */
 	public  void move(Node node, int L, int l) {
 		Vector3 dir = direction(pos, target);
 		dir.normalize();
@@ -29,14 +49,19 @@ public class Movement {
 		pos.y += dir.data[1];
 		
 		node.setAttribute("xy", pos.x, pos.y);
-		
+		//Si le noeud est proche de sa cible, on détermine une nouvelle destination
 		if(target.distance(pos) < speed) {
 			target.set(random.nextDouble()*L, random.nextDouble()*l);
 		}
 	}
 	
-	public Vector3 direction(Point3 a, Point3 b) {
-		return new Vector3(b.x - a.x, b.y - a.y, b.z - a.z);
+	/**
+	 * Détermine le vecteur direction entre la position et la cible.
+	 * @param pos La position courante
+	 * @param target La destination
+	 */
+	public Vector3 direction(Point3 pos, Point3 target) {
+		return new Vector3(target.x - pos.x, target.y - pos.y, targt.z - pos.z);
 	}
 	
 	public String toString() {
